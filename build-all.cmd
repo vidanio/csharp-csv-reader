024f41a2141426817ae6f9a41ec6e65484c70fd2
Ted Spence <ted@spence.net>
2016-11-02 20:29:05 -0700

    Implemented a build-all script

---
+++ build-all.cmd
@@ -0,0 +1,6 @@
+c:\windows\microsoft.net\Framework64\v4.0.30319\msbuild.exe csvfile\csvfile.csproj /property:Configuration=Debug-DotNet35
+c:\windows\microsoft.net\Framework64\v4.0.30319\msbuild.exe csvfile\csvfile.csproj /property:Configuration=Debug-DotNet40
+c:\windows\microsoft.net\Framework64\v4.0.30319\msbuild.exe csvfile\csvfile.csproj /property:Configuration=Debug-DotNet45
+c:\windows\microsoft.net\Framework64\v4.0.30319\msbuild.exe csvfile\csvfile.csproj /property:Configuration=Release-DotNet35
+c:\windows\microsoft.net\Framework64\v4.0.30319\msbuild.exe csvfile\csvfile.csproj /property:Configuration=Release-DotNet40
+c:\windows\microsoft.net\Framework64\v4.0.30319\msbuild.exe csvfile\csvfile.csproj /property:Configuration=Release-DotNet45
